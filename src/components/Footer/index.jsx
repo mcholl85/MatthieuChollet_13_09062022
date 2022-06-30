b0ba44d01b38bee8b8341e27cadacfeab4b8d414
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2022-06-30 16:35:36 +0200

    feat: change to typescript

--- src/components/Footer/index.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer-text">Copyright 2020 Argent Bank</p>
-    </footer>
-  );
-}
--- src/utils/store.js
+++
@@ -1,8 +0,0 @@
-import userReducer from '../features/user'
-import { configureStore } from '@reduxjs/toolkit'
-
-export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
