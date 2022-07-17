f1c57aeb1f97abaf6cc7996c732f82b84e147010
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2022-07-17 21:29:30 +0200

    fix: remove react-query

--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,14 @@ import 'font-awesome/css/font-awesome.min.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './utils/store'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <React.StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </React.StrictMode>
-    </Provider>
-  </QueryClientProvider>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
 )
